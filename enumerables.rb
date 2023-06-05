dbe31527e69244e51a3a1c6ea5d48fd4e2a74abf
Julio Uribe <uribejulioc@gmail.com>
2023-06-05 15:02:00 -0700

    Add my_rotate

--- enumerables.rb
+++ enumerables.rb
@@ -48,6 +48,32 @@ class Array
 	flattened
   end
 
-  def my_zip()
+  def my_zip(*arrays)
+    merged = []
+    self.each_with_index do |ele, idx|
+      sub_array = [ele]
+      arrays.my_each do |array|
+        sub_array << array[idx]
+      end
+      merged << sub_array
+    end
+    merged
   end
+
+  def my_rotate(rotations=1)
+    arr = []
+    self.each {|ele| arr << ele }
+    rotations.abs.times do |rotation|
+      if rotations > 0
+          first = arr.shift
+          arr << first
+      else
+          last = arr.pop
+          arr.unshift(last)
+      end
+    end
+    arr
+  end
+
+  
 end
