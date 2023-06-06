fe2e1a44a0d99e57db88b0f21907da80962f1467
Julio Uribe <uribejulioc@gmail.com>
2023-06-05 17:14:27 -0700

    Add individual players and plan out 'play_round'

--- Ghost/game.rb
+++ Ghost/game.rb
@@ -1,7 +1,12 @@
+require_relative("player")
+
 class Game
 	attr_reader :dictionary
-	def initialize(players, fragment, dictionary)
-		@players = ""
+
+	def initialize(fragment, dictionary)
+		@player_1 = Player.new("Player 1")
+		@player_2 = Player.new("Player 2")
+		@current_player = @player_1
 		@fragment = ""
 		@dictionary = Hash.new(false)
 
@@ -12,7 +17,15 @@ class Game
 		file.close
 	end
 
-	
+	def play_round
+		"""
+		0) check substring against dictionary words and find valid letters, valid_guesses
+		1) call 'take_turn' to get input
+		2) add the letter to fragment
+		3) check against the dictionary
+		4) next player! updates current player and previous player.
+		"""
+	end
 end
 
 if __FILE__ == $PROGRAM_NAME
--- Ghost/player.rb
+++ Ghost/player.rb
@@ -5,13 +5,25 @@ class Player
 		@name = name
 	end
 
-	def guess
-		puts 'Enter a letter: '
+	def guess(valid_guesses)
+		puts self.name + ', please enter a letter: '
 		input = gets.chomp
+		abc = ('a'..'z')
 		# input has to be 1 char long
 		# char has to be part of alphabet
-		# can guess char that was already guessed
-		# find valid substrings for fragment
-		# .uniq 
+		if input.len != 1 or !abc.include?(input)
+			puts 'Enter a single letter'
+			# find valid substrings for fragment
+			# .uniq
+			# can guess char that was already guessed
+		elsif !valid_guesses.include?(input)
+			self.alert_invalid_guess(input)
+		else
+			input
+		end
+	end
+
+	def alert_invalid_guess(input)
+		puts input + " is not a valid choice."
 	end
 end
